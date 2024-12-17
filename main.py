84193b193af7d9f9d83c6561540db5e3c8179128
Stan <kravcivs717@gmail.com>
2024-12-17 22:54:02 +0100

    Day3_part1_update

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import re
 
+with open("data", "r", encoding="utf-8") as file:
+    memory = file.read()
+
 
 def mul(a, b):
     return a * b
 
 
-count = 0
+def find_mul(content):
+    count = 0
+    list_functions = []
+    position = re.findall(r'mul\((-?\d+),\s*(-?\d+)\)', content)
+    for i in range(len(position)):
+        list_functions.append(list(position[i]))
 
-with open("data", "r", encoding="utf-8") as file:
-    content = file.read()
+    for i in range(len(list_functions)):
+        list_functions[i][0] = int(list_functions[i][0])
+        list_functions[i][1] = int(list_functions[i][1])
 
-position = re.findall(r'mul\((-?\d+),\s*(-?\d+)\)', content)
-print(f"Found {len(position)} matches: {position}")
-print(type(position[0]))
+    for i in range(len(list_functions)):
+        count += mul(list_functions[i][0], list_functions[i][1])
 
-list_functions = []
-for i in range(len(position)):
-    list_functions.append(list(position[i]))
-
-for i in range(len(list_functions)):
-    list_functions[i][0] = int(list_functions[i][0])
-    list_functions[i][1] = int(list_functions[i][1])
-
-for i in range(len(list_functions)):
-    count += mul(list_functions[i][0], list_functions[i][1])
-
-print(count)
+    return count
+print(find_mul(memory))
