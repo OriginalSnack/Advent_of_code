845942a229eea4e28d0d3379352b09e9577c9ae5
Stan <kravcivs717@gmail.com>
2024-12-17 20:38:12 +0100

    Day3_part1

--- main.py
+++ main.py
@@ -1,41 +1,28 @@
-data = """ 
-"""
+import re
 
 
-def function_change(unsafe, j):
-    if j >= len(unsafe):
-        return False
-    temp_list = unsafe.copy()
-    temp_list.pop(j)
-    if element_check_safe(temp_list):
-        return True
-    else:
-        return function_change(unsafe, j + 1)
+def mul(a, b):
+    return a * b
 
 
-def element_check_safe(element):
-    is_descending = all(element[i] > element[i + 1] for i in range(len(element) - 1))
-    is_increasing = all(element[i] < element[i + 1] for i in range(len(element) - 1))
-    is_different = all(1 <= abs(element[i] - element[i + 1]) <= 3 for i in range(len(element) - 1))
-    if is_different and is_increasing != is_descending:
-        return True
-    else:
-        return False
+count = 0
 
-def function_check(dat):
-    count = 0
-    list_unsafe = []
-    data_list = [[int(num) for num in row.split()] for row in dat.splitlines()]
-    for i in range(len(data_list)):
-        if element_check_safe(data_list[i]):
-            count += 1
-        else:
-            list_unsafe.append(data_list[i])
-    return list_unsafe,count
+with open("data", "r", encoding="utf-8") as file:
+    content = file.read()
 
-light_list,count = function_check(data)
-for i in range(len(light_list)):
-    if function_change(light_list[i],0):
-        count+=1
+position = re.findall(r'mul\((-?\d+),\s*(-?\d+)\)', content)
+print(f"Found {len(position)} matches: {position}")
+print(type(position[0]))
+
+list_functions = []
+for i in range(len(position)):
+    list_functions.append(list(position[i]))
+
+for i in range(len(list_functions)):
+    list_functions[i][0] = int(list_functions[i][0])
+    list_functions[i][1] = int(list_functions[i][1])
+
+for i in range(len(list_functions)):
+    count += mul(list_functions[i][0], list_functions[i][1])
+
 print(count)
-
